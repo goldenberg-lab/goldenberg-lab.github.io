fc71edae4e75f468414b03fc5fd875d0fde6953b
Ben Brew <benmbrew@gmail.com>
2018-10-29 15:27:04 -0400

    resturcturing current research section and adding new papers and replacing images

--- code/resize_code.py
+++ code/resize_code.py
@@ -7,9 +7,9 @@ os.chdir('/home/benbrew/Desktop/pics')
 
 
 for number in range(2)[1:]:
-    img = Image.open(str(number) + '.jpg')
-    img = img.resize((180, 209), PIL.Image.ANTIALIAS)
-    img.save(str(number) + '_new.jpg')
+    img = Image.open(str(number) + '.png')
+    img = img.resize((600, 400), PIL.Image.ANTIALIAS)
+    img.save(str(number) + '_big.png')
 
 
 
