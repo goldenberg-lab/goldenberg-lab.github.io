149bc420e7cfef6382df5b2642d51ff0081dd588
benmbrew <benmbrew@gmail.com>
2019-05-03 13:52:09 -0400

    added dustin back to lab page

--- code/resize_code.py
+++ code/resize_code.py
@@ -3,13 +3,13 @@
 import PIL
 from PIL import Image
 import os
-os.chdir('/home/benbrew/Desktop/pics')
+os.chdir('/home/ben/Desktop/lab_pics')
 
 
 for number in range(2)[1:]:
     img = Image.open(str(number) + '.png')
-    img = img.resize((600, 400), PIL.Image.ANTIALIAS)
-    img.save(str(number) + '_big.png')
+    img = img.resize((180, 205), PIL.Image.ANTIALIAS)
+    img.save(str(number) + '_new.png')
 
 
 
