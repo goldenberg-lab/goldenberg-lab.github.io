3919c8a00618264e1b8f9e35f30afc99e643dca4
benmbrew <benmbrew@gmail.com>
2019-05-03 16:15:12 -0400

    added shalmali to the team page

--- code/resize_code.py
+++ code/resize_code.py
@@ -7,9 +7,9 @@ os.chdir('/home/ben/Desktop/lab_pics')
 
 
 for number in range(2)[1:]:
-    img = Image.open(str(number) + '.png')
+    img = Image.open(str(number) + '.jpg')
     img = img.resize((180, 205), PIL.Image.ANTIALIAS)
-    img.save(str(number) + '_new.png')
+    img.save(str(number) + '_new.jpg')
 
 
 
