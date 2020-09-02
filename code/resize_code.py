1481bf738c7d0dc1a5e37ae5ee5c517b3b146ab3
benmbrew <benmbrew@gmail.com>
2020-09-02 13:32:28 -0400

    added two projects to current project page

--- code/resize_code.py
+++ code/resize_code.py
@@ -3,13 +3,13 @@
 import PIL
 from PIL import Image
 import os
-os.chdir('/home/ben/Desktop/pics')
+os.chdir('/home/benbrew88/Desktop/pics')
 
 
-for number in range(2)[1:]:
+for number in range(3)[1:]:
     img = Image.open(str(number) + '.jpg')
-    img = img.resize((180, 205), PIL.Image.ANTIALIAS)
-    img.save(str(number) + '_new.jpg')
+    img = img.resize((600, 400), PIL.Image.ANTIALIAS)
+    img.save(str(number) + '_big.jpg')
 
 
 
