74387e995dfd58d24786359d71e200898d22c8c2
benmbrew <benmbrew@gmail.com>
2020-12-23 18:22:25 -0500

    added covid ER presearch to current research and linked to video and paper

--- code/resize_code.py
+++ code/resize_code.py
@@ -6,10 +6,10 @@ import os
 os.chdir('/home/benbrew88/Desktop/pics')
 
 
-for number in range(3)[1:]:
-    img = Image.open(str(number) + '.jpg')
+for number in range(1)[1:]:
+    img = Image.open(str(number) + '.png')
     img = img.resize((600, 400), PIL.Image.ANTIALIAS)
-    img.save(str(number) + '_big.jpg')
+    img.save(str(number) + '_big.png')
 
 
 
